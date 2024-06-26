5b1bf42bdc750808caf32a5bbab87d3f2b4b17d3
Palma97 <palmabboy3@gmail.com>
2024-06-26 10:50:26 -0500

    13 commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,22 @@ import "./App.css";
 import { Home } from "./components/Home/Home.jsx";
 import Projects from "./components/Projects/Projects.jsx";
 import Studies from "./components/Studies/Studies.jsx"; */
-import { AnimatePresence } from "framer-motion";
+/* import { AnimatePresence } from "framer-motion"; */
 
 function App() {
   /* const location = useLocation(); */
   return (
     <div className="App">
-      <AnimatePresence>
-        {/* <Routes location={location} key={location.pathname}>
+      {/* <AnimatePresence> */}
+      {/* <Routes location={location} key={location.pathname}>
           <Route path="/" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/studies" element={<Studies />} />
         </Routes> */}
-        Hola mundo
-      </AnimatePresence>
+      Hola mundo
+      {/* </AnimatePresence> */}
     </div>
   );
 }
