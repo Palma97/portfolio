a476302b17bf299531dbfac18860eb3a266e1a3c
Palma97 <palmabboy3@gmail.com>
2024-06-26 08:58:41 -0500

    First commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,42 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+/* import i18next from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import en from "./i18n/en.json";
+import es from "./i18n/es.json"; */
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+/* i18next
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    lng: "es",
+    interpolation: {
+      escapeValue: false,
+    },
+    resources: {
+      en: {
+        translation: en,
+      },
+      es: {
+        translation: es,
+      },
+    },
+    fallbackLng: "en",
+    detection: {
+      order: ["queryString", "cookie"],
+      cache: ["cookie"],
+    },
+  }); */
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
