0f536c56805759b213be694c8e98333a9e44d121
Palma97 <palmabboy3@gmail.com>
2024-06-26 11:07:55 -0500

    14 commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,25 @@
 import "./App.css";
-/* import { Route, Routes, useLocation } from "react-router-dom"; */
-/* import About from "./components/About/About.jsx"; */
+import { Route, Routes, useLocation } from "react-router-dom";
+import About from "./components/About/About.jsx";
 /* import Contact from "./components/Contact/Contact.jsx";
 import { Home } from "./components/Home/Home.jsx";
 import Projects from "./components/Projects/Projects.jsx";
 import Studies from "./components/Studies/Studies.jsx"; */
-/* import { AnimatePresence } from "framer-motion"; */
+import { AnimatePresence } from "framer-motion";
 
 function App() {
-  /* const location = useLocation(); */
+  const location = useLocation();
   return (
     <div className="App">
-      {/* <AnimatePresence> */}
-      {/* <Routes location={location} key={location.pathname}>
-          <Route path="/" element={<Home />} />
-          <Route path="/projects" element={<Projects />} />
+      <AnimatePresence>
+        <Routes location={location} key={location.pathname}>
+          {/* <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} /> */}
           <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/studies" element={<Studies />} />
-        </Routes> */}
-      Hola mundo
-      {/* </AnimatePresence> */}
+          {/* <Route path="/contact" element={<Contact />} />
+          <Route path="/studies" element={<Studies />} /> */}
+        </Routes>
+      </AnimatePresence>
     </div>
   );
 }
