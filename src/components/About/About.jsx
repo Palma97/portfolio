9c50d28015608b9fa04e0a57d84e0f7ab13b2bf4
Palma97 <palmabboy3@gmail.com>
2024-06-26 10:31:39 -0500

    11 commit

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,6 +1,6 @@
 import "./About.css";
 import htmlFondo from "../../Images/htmlFondo.png";
-import { motion } from "framer-motion";
+/* import { motion } from "framer-motion"; */
 import { IoIosLink } from "react-icons/io";
 import { useTranslation } from "react-i18next";
 
@@ -8,12 +8,13 @@ function About() {
   const { t } = useTranslation();
 
   return (
-    <motion.div
+    <div>
+      {/* <motion.div
       className="containerr"
       initial={{ opacity: 0, transition: { duration: 0.5 } }}
       animate={{ opacity: 1, transition: { duration: 0.5 } }}
       exit={{ opacity: 0, transition: { duration: 0.5 } }}
-    >
+    > */}
       <div className="smallContainer">
         <h1 className="title">{t("BUTTONABOUT")}</h1>
         <div className="textAbout">{t("TEXTABOUT")}</div>
@@ -29,7 +30,8 @@ function About() {
       <div className="htmlFondo">
         <img className="imgHtml" src={htmlFondo} alt="Not found" />
       </div>
-    </motion.div>
+      {/* </motion.div> */}
+    </div>
   );
 }
 
